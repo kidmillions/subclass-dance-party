40a059f83f527383acda7f82127b99d8493a2f4f
kidmillions <claymanchris@gmail.com>
2015-05-20 15:55:50 -0700

    Refactored Dancer functions into pseudoclassical pattern

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,3 +1,23 @@
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+
+};
+
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer;
+
+BlinkyDancer.prototype.oldStep = Dancer.prototype.step;
+BlinkyDancer.prototype.step = function() {
+  this.oldStep();
+  console.log(this.$node);
+  this.$node.toggle();
+};
+
+
+
+
+
+
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
